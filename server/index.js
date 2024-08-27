e3a8c39a9a9512c93d3cbfe26ee15f31f3f9db8f
Taha Iftikhar <tiftikhar@student.iul.ac.in>
2024-08-27 20:26:58 +0530

    Revert "error fixes"
    This reverts commit a5ca6148cb78a573674b853eceb457cab09b4cce.

--- server/index.js
+++ server/index.js
@@ -55,7 +55,7 @@ app.use("/posts", postRoutes);
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-  .connect(`${process.env.MONGO_URL}/${process.env.DATABSE_URL}`, {
+  .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
